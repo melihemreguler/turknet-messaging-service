455fabefe95d5d3f39d0d99063395235e013a75e
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-25 20:34:28 +0300

    Initial commit

---
+++ src/main/java/com/github/melihemreguler/turknetmessagingservice/TurknetMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.melihemreguler.turknetmessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurknetMessagingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurknetMessagingServiceApplication.class, args);
+    }
+
+}
