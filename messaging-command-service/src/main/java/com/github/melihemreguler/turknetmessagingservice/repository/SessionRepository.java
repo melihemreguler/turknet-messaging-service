51047f9a8f651c6af0bba6625b70df7f36b3aa44
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-28 01:10:48 +0300

    feat: Add method to find sessions by user ID in SessionRepository

--- messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/repository/SessionRepository.java
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/repository/SessionRepository.java
@@ -4,5 +4,9 @@ import com.github.melihemreguler.turknetmessagingservice.dto.SessionDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SessionRepository extends MongoRepository<SessionDto, String> {}
+public interface SessionRepository extends MongoRepository<SessionDto, String> {
+    List<SessionDto> findByUserId(String userId);
+}
