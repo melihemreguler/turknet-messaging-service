55ecf740b64fcd41f53448d907a3461b673bc8cb
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-28 19:39:06 +0300

    feat: implement ActivityLogRepository for managing activity logs with pagination support

---
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/repository/ActivityLogRepository.java
@@ -0,0 +1,24 @@
+package com.github.melihemreguler.turknetmessagingservice.repository;
+
+import com.github.melihemreguler.turknetmessagingservice.dto.ActivityLogDto;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ActivityLogRepository extends MongoRepository<ActivityLogDto, String> {
+    Optional<ActivityLogDto> findByUserId(String userId);
+    
+    @Query(value = "{'userId': ?0}", fields = "{'logs': {$slice: [?1, ?2]}}")
+    Optional<ActivityLogDto> findByUserIdWithPagination(String userId, int offset, int limit);
+    
+    @Aggregation(pipeline = {
+        "{ $match: { 'userId': ?0 } }",
+        "{ $project: { 'totalLogs': { $size: '$logs' } } }"
+    })
+    Optional<Integer> countLogsByUserId(String userId);
+}
