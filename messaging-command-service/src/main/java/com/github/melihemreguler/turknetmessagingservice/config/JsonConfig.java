3fc0099b91b8d6ead733eca79df4b6f346894b80
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-29 17:37:23 +0300

    feat: add JsonConfig for custom ObjectMapper with JavaTimeModule

---
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/config/JsonConfig.java
@@ -0,0 +1,18 @@
+package com.github.melihemreguler.turknetmessagingservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
