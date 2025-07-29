173680ede7eabb5dc3f28e34ef560fb8e57f6010
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-29 23:56:53 +0300

    feat: add BaseTurknetMessagingException class for custom exception handling

---
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/exception/BaseTurknetMessagingException.java
@@ -0,0 +1,62 @@
+package com.github.melihemreguler.turknetmessagingservice.exception;
+
+import lombok.Getter;
+
+/**
+ * Base exception class for all Turknet Messaging Service related exceptions.
+ * All custom exceptions in the messaging command service should extend this class.
+ */
+@Getter
+public abstract class BaseTurknetMessagingException extends RuntimeException {
+    
+    private final String errorCode;
+    private final String originalMessage;
+    
+    protected BaseTurknetMessagingException(String message) {
+        super(message);
+        this.errorCode = getDefaultErrorCode();
+        this.originalMessage = null;
+    }
+    
+    protected BaseTurknetMessagingException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+        this.originalMessage = null;
+    }
+    
+    protected BaseTurknetMessagingException(String message, String errorCode, String originalMessage) {
+        super(message);
+        this.errorCode = errorCode;
+        this.originalMessage = originalMessage;
+    }
+    
+    protected BaseTurknetMessagingException(String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = getDefaultErrorCode();
+        this.originalMessage = null;
+    }
+    
+    protected BaseTurknetMessagingException(String message, String errorCode, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+        this.originalMessage = null;
+    }
+    
+    protected BaseTurknetMessagingException(String message, String errorCode, String originalMessage, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+        this.originalMessage = originalMessage;
+    }
+    
+    /**
+     * Returns the default error code for this exception type.
+     * Should be overridden by subclasses to provide specific error codes.
+     */
+    protected abstract String getDefaultErrorCode();
+    
+    /**
+     * Returns a short description of the exception type.
+     * Used for logging and monitoring purposes.
+     */
+    public abstract String getExceptionType();
+}
