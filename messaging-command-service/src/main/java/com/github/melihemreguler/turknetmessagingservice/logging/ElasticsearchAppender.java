6f74cc1108682177936ab019eeb0bb6302f3ee51
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-29 17:37:49 +0300

    refactor: remove hardcoded Elasticsearch configuration and add setters for dynamic configuration

--- messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/logging/ElasticsearchAppender.java
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/logging/ElasticsearchAppender.java
@@ -2,7 +2,7 @@ package com.github.melihemreguler.turknetmessagingservice.logging;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Setter;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -11,55 +11,49 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ElasticsearchAppender extends AppenderBase<ILoggingEvent> {
-    
+
     private static final Logger LOGGER = Logger.getLogger(ElasticsearchAppender.class.getName());
+
+    @Setter
+    private String elasticsearchUrl;
+
+    @Setter
+    private String indexName;
     
-    private String elasticsearchUrl = "http://elasticsearch:9200";
-    private String indexName = "turknet-messaging-logs";
     private WebClient webClient;
-    private ObjectMapper objectMapper = new ObjectMapper();
-    
+
     @Override
     public void start() {
         super.start();
         this.webClient = WebClient.builder()
-            .baseUrl(elasticsearchUrl)
-            .build();
+                .baseUrl(elasticsearchUrl)
+                .build();
     }
-    
+
     @Override
     protected void append(ILoggingEvent eventObject) {
         try {
             Map<String, Object> loggingEvent = createLogDocument(eventObject);
-            
+
             webClient.post()
-                .uri("/{indexName}/_doc", indexName)
-                .bodyValue(loggingEvent)
-                .retrieve()
-                .bodyToMono(Void.class)
-                .onErrorResume(exception -> {
-                    LOGGER.log(Level.WARNING, "Unable to send log to Elasticsearch: " + exception.getMessage());
-                    return Mono.empty();
-                })
-                .subscribe();
-                
+                    .uri("/{indexName}/_doc", indexName)
+                    .bodyValue(loggingEvent)
+                    .retrieve()
+                    .bodyToMono(Void.class)
+                    .onErrorResume(exception -> {
+                        LOGGER.log(Level.WARNING, "Unable to send log to Elasticsearch: " + exception.getMessage());
+                        return Mono.empty();
+                    })
+                    .subscribe();
+
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "Error creating log document for Elasticsearch", e);
         }
     }
-    
+
     private Map<String, Object> createLogDocument(ILoggingEvent event) {
         return LogDocumentBuilder.create()
                 .fromEvent(event, "messaging-command-service")
                 .build();
-    }
-    
-    // Setters for configuration
-    public void setElasticsearchUrl(String elasticsearchUrl) {
-        this.elasticsearchUrl = elasticsearchUrl;
-    }
-    
-    public void setIndexName(String indexName) {
-        this.indexName = indexName;
     }
 }
