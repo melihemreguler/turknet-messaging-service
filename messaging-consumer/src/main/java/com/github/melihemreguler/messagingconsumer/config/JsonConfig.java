2b9b4318679ece69208292cf4aca4bb4d3852526
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-29 17:39:02 +0300

    feat: add JsonConfig class for custom ObjectMapper with JavaTimeModule support

---
+++ messaging-consumer/src/main/java/com/github/melihemreguler/messagingconsumer/config/JsonConfig.java
@@ -0,0 +1,19 @@
+package com.github.melihemreguler.messagingconsumer.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JsonConfig {
+
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
