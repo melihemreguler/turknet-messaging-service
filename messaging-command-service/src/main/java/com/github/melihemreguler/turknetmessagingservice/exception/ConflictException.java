69c54765888a18df67bf9f668ef9649a86c3de3f
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-29 23:56:58 +0300

    refactor: change ConflictException to extend BaseTurknetMessagingException and add error code handling

--- messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/exception/ConflictException.java
+++ messaging-command-service/src/main/java/com/github/melihemreguler/turknetmessagingservice/exception/ConflictException.java
@@ -1,11 +1,22 @@
 package com.github.melihemreguler.turknetmessagingservice.exception;
 
-public class ConflictException extends RuntimeException {
+public class ConflictException extends BaseTurknetMessagingException {
+    
     public ConflictException(String message) {
         super(message);
     }
     
     public ConflictException(String message, Throwable cause) {
         super(message, cause);
+    }
+    
+    @Override
+    protected String getDefaultErrorCode() {
+        return "CONFLICT_ERROR";
+    }
+    
+    @Override
+    public String getExceptionType() {
+        return "ConflictException";
     }
 }
