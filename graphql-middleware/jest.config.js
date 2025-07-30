9af8ea5af48c3b1afead2eea20d076dfa0ab5efd
Melih Emre Güler <gulermelihemre@gmail.com>
2025-07-30 16:02:02 +0300

    feat: add Jest configuration for graphql-middleware

---
+++ graphql-middleware/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+  testTimeout: 30000,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+};
